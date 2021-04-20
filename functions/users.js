83911810c60370a57105138ab179a35703b2f954
Rumit Rout <rumitr@gmail.com>
2021-04-20 11:51:47 +0530

    Added search query

--- functions/users.js
+++ functions/users.js
@@ -1,15 +1,23 @@
 const faker = require("faker");
-function getUsers(page, size) {
+function getUsers(page, size, search) {
   let users = [];
   for (let i = (page - 1) * size; i < page * size; i++) {
-    users.push(getUser(i));
+    users.push(getUser(i, search));
   }
   return users;
 }
-function getUser(index) {
+
+function contains(text) {
+  let name = faker.name.firstName() + " " + faker.name.lastName();
+  if (name.includes(text)) return name;
+  else {
+    return contains(text);
+  }
+}
+function getUser(index, search = "") {
   let user = {};
   user.id = index + 1;
-  user.name = faker.name.firstName() + " " + faker.name.lastName();
+  user.name = contains(search);
   user.jobTitle = faker.name.jobTitle();
   user.prefix = faker.name.prefix();
   user.suffix = faker.name.suffix();
@@ -25,11 +33,16 @@ const headers = {
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
 };
 exports.handler = async function (event, context) {
-  const { page = 1, size = 11 } = event.queryStringParameters;
+  const { page = 1, size = 11, search = "" } = event.queryStringParameters;
   const total = Math.ceil(10000 / size);
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify({ page, total, size, data: getUsers(page, size) }),
+    body: JSON.stringify({
+      page,
+      total,
+      size,
+      data: getUsers(page, size, search),
+    }),
   };
 };
